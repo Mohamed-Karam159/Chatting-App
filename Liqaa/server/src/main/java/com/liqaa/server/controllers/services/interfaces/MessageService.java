5b0c8b5d13479a06dc07e5e445fb1c4428b19127
Hazim Karam <hazimkaram@gmail.com>
2025-02-05 16:39:09 +0200

    Write MessageService Interface and MessageServiceImpl

---
+++ Liqaa/server/src/main/java/com/liqaa/server/controllers/services/interfaces/MessageService.java
@@ -0,0 +1,12 @@
+package com.liqaa.server.controllers.services.interfaces;
+
+import com.liqaa.shared.models.entities.Message;
+import java.util.List;
+
+public interface MessageService {
+    List<Message> getMessagesByConversationId(int conversationId); // Without pagination
+    List<Message> getMessagesByConversationId(int conversationId, int offset, int limit); // With pagination
+    void sendMessage(Message message); // Send a message
+    int getUnreadMessageCount(int conversationId, int userId); // Get unread message count
+    void markMessagesAsSeen(int conversationId, int userId); // Mark messages as seen
+}
